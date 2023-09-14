93aa1a18d8f0b9b52a254188cbf00d78756c747f
hjyoon66 <poiu601@naver.com>
2023-09-15 03:56:11 +0900

    CORS Problem solved

--- react-workspace/todo-react-app/src/App.js
+++ react-workspace/todo-react-app/src/App.js
@@ -1,32 +1,36 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Todo from './Todo';
 import AddTodo from './AddTodo'
 import {Container, List, Paper} from "@mui/material"
+import {call} from "./service/ApiService"
 
 function App() {
     const [items, setItems] = useState([]);
 
-    const addItem = (item) =>{
-        item.id = "ID-"+items.length;
-        item.done=false;
-        setItems([...items,item]);
-        console.log("items = ", items);
+    useEffect(() => {
+        call("/todo", "GET", null)
+            .then((response) => setItems(response.data));
+    },[]);
+
+    const addItem = (item) => {
+        call("/todo","POST",item)
+            .then((response)=>setItems(response.data));
     }
 
-    const deleteItem = (item)=>{
-        const newItems = items.filter(e => e.id !== item.id);
-        setItems([...newItems]);
+    const deleteItem = (item) => {
+        call("/todo","DELETE",item)
+            .then((response)=>setItems(response.data));
     }
 
-    const editItem = () =>{
-        setItems([...items])
-    }
+    const editItem = (item) => {
+        call("/todo","PUT", item)
+            .then((response)=>setItems(response.data));    }
 
     let todoItems = items.length > 0 && (
-        <Paper style={{margin:16}}>
+        <Paper style={{margin: 16}}>
             <List>
-                {items.map((item) =>(
-                <Todo item={item} key={item.id} deleteItem={deleteItem} editItem={editItem}/>
+                {items.map((item) => (
+                    <Todo item={item} key={item.id} deleteItem={deleteItem} editItem={editItem}/>
                 ))}
             </List>
         </Paper>

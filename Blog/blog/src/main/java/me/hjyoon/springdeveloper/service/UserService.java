3a73ef0875496075952c400e70ae2f830d12500e
hjYoon66 <poiu601@naver.com>
2024-01-29 16:23:10 +0900

    Spring Security work :running:

---
+++ Blog/blog/src/main/java/me/hjyoon/springdeveloper/service/UserService.java
@@ -0,0 +1,22 @@
+package me.hjyoon.springdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.hjyoon.springdeveloper.domain.User;
+import me.hjyoon.springdeveloper.dto.AddUserRequest;
+import me.hjyoon.springdeveloper.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto){
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}

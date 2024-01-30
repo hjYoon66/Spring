376bbbb403656ee333942ae68c3827b957de79db
hjYoon66 <poiu601@naver.com>
2024-01-30 09:58:26 +0900

    JWT Token work :running:

---
+++ Blog/blog/src/main/java/me/hjyoon/springdeveloper/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package me.hjyoon.springdeveloper.repository;
+
+import me.hjyoon.springdeveloper.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long>{
+    Optional<RefreshToken> findByUserId(Long userId);
+
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}

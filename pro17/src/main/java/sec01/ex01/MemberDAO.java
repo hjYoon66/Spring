1084d42d044ef0d401e14da64496221a849616d6
hjyoon66 <poiu601@naver.com>
2024-06-15 23:43:32 +0900

    pro17 start :zap:

---
+++ pro17/src/main/java/sec01/ex01/MemberDAO.java
@@ -0,0 +1,49 @@
+package sec01.ex01;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberDAO {
+    private DataSource dataFactory;
+    private Connection conn;
+    private PreparedStatement pstmt;
+
+    public MemberDAO() {
+        try{
+            Context ctx = new InitialContext();
+            Context envContext = (Context) ctx.lookup("java:/comp/env");
+            dataFactory = (DataSource) envContext.lookup("jdbc:/oracle");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public List<MemberVO> listMembers(){
+        List<MemberVO> membersList = new ArrayList<>();
+        try{
+            conn = dataFactory.getConnection();
+            String query = "select * from t_member order by joinDate desc";
+            System.out.println(query);
+            ResultSet rs = pstmt.executeQuery();
+            while(rs.next()){
+                String id = rs.getString("id");
+                String pwd = rs.getString("pwd");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                Date joinDate = rs.getDate("joinDate");
+                MemberVO memberVO = new MemberVO(id, pwd, name, email, joinDate);
+                membersList.add(memberVO);
+            }
+            rs.close();
+            pstmt.close();
+            conn.close();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return membersList;
+    }
+}
+
---
+++ pro17/src/main/webapp/test01/listMembers.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: poiu601
+  Date: 2024-06-15
+  Time: 오후 2:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>

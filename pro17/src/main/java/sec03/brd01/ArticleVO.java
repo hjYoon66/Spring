0dd8acf581492c2e81b96eb502ad8e04c3dbecc9
hjyoon66 <poiu601@naver.com>
2024-06-17 00:46:03 +0900

    Board Imple -ing :zap:

---
+++ pro17/src/main/java/sec03/brd01/ArticleVO.java
@@ -0,0 +1,56 @@
+package sec03.brd01;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ArticleVO {
+    private int level;
+    private int articleNo;
+    private int parentNo;
+    private String title;
+    private String content;
+    private String imageFileName;
+    private String id;
+    private Date writeDate;
+
+    public ArticleVO(int level, int articleNo, int parentNo, String title, String content, String imageFileName, String id) {
+        this.level = level;
+        this.articleNo = articleNo;
+        this.parentNo = parentNo;
+        this.title = title;
+        this.content = content;
+        this.imageFileName = imageFileName;
+        this.id = id;
+    }
+    public String getImageFileName() {
+        try {
+            if (imageFileName != null && imageFileName.length() != 0) {
+                imageFileName = URLDecoder.decode(imageFileName, "UTF-8");
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return imageFileName;
+    }
+
+    public void setImageFileName(String imageFileName) {
+        try {
+            if (imageFileName != null && imageFileName.length() != 0) {
+                this.imageFileName = URLEncoder.encode(imageFileName, "UTF-8");//파일이름에 특수문자가 있을 경우 인코딩합니다.
+            }else {
+                this.imageFileName = imageFileName;
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ pro17/src/main/java/sec03/brd01/BoardController.java
@@ -0,0 +1,56 @@
+package sec03.brd01;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/board/*")
+public class BoardController extends HttpServlet {
+    BoardService boardService;
+    ArticleVO articleVO;
+
+    @Override
+    public void init() throws ServletException {
+        boardService = new BoardService();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doHandle(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doHandle(req, resp);
+    }
+
+    private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        String nextPage = "";
+        request.setCharacterEncoding("utf-8");
+        response.setContentType("text/html; charset=utf-8");
+        String action = request.getPathInfo();
+        System.out.println("action = " + action);
+        try{
+            List<ArticleVO> articlesList = new ArrayList<>();
+            if(action == null){
+                articlesList = boardService.listArticles();
+                request.setAttribute("articlesList", articlesList);
+                nextPage = "/board01/listArticles.jsp";
+            } else if (action.equals("/listArticles.do")) {
+                articlesList = boardService.listArticles();
+                request.setAttribute("articlesList", articlesList);
+                nextPage = "/board01/listArticles.jsp";
+            }
+            RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
+            dispatch.forward(request, response);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
